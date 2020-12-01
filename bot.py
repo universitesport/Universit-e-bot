dd966dfd3eb4019ec78f259b75a3629c3cdcf118
Spitch270 <felicien.cuny@outlook.fr>
2020-12-01 16:06:25 +0100

    ajout commande

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@
 #Copyright : This code can be use for every member working on 'Universit'E-Bot' discord bot only. If you want to work on it, please contact me : felicien.cuny@outlook.fr
 
 import discord #importation des modules discord
+import random  #importation du random
 from discord.ext import commands #importation des fonctions pour discord bot
 
 universitebot = commands.Bot(command_prefix = '$') #prefixe à utiliser pour intéragir avec le bot
@@ -13,16 +14,12 @@ async def on_ready(): #lancement du bot en ligne
     print('Le bot est pret') #indique que le bot est lancé.
 
 @universitebot.command() #Déclarer à chaque fois que l'on veut utiliser le bot
-async def ping(trigger): #ping = nom de la commande; exemple $ping pourra être utiliser sur discord pour trigger le bot
-    await trigger.send("Pong !")
+async def ping(ctx): #ping = nom de la commande; exemple $ping pourra être utiliser sur discord pour trigger le bot
+    await ctx.send("Pong !")
 
 @universitebot.command()
-async def sardoche(trigger): #$sardoche envoie random une citation de sardoche
-    responses = ["Mais c'était sur en faite !"
-                 "Il est mort, le noir est mort"
+async def sardoche(ctx): #$sardoche envoie random une citation de sardoche
+    reponses = ["Mais c'était sur en faite !",
+                 "Il est mort, le noir est mort",
                  "POURQUOI ? POURQUOI ? POURQUOI ?"]
-    await trigger.send("f'{random.choice(responses)}") #commande chelou j'ai pas compris
-
-
-
-universitebot.run('MjkyNjg5OTgwMjE4NjcxMTA1.WM1ayQ.opffC2LIpeUoarM9EsbOdc8Jw1w') #token utilisé pour le bot
+    await ctx.send(f'{random.choice(reponses)}') #envoie la réponse 
